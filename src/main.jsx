d02183d8ce9050f0f1e8400670ab392412c1a8f2
Bipin Ghimire <beepinghimire2022@gmail.com>
2025-04-03 20:01:16 +0545

    Third update

--- src/main.jsx
+++ src/main.jsx
@@ -10,13 +10,13 @@ import { CartProvider } from './Context/CartProvider.jsx';
 createRoot(document.getElementById('root')).render(
  <BrowserRouter>
  <CartProvider>
-<Auth0Provider
-   domain="dev-0kpjrl1vlnoxp0g3.us.auth0.com"
-   clientId="Cdt7Z8eXFKaHwnPrB6kAE4PommkyermK"
-   authorizationParams={{
-     redirect_uri: window.location.origin
-   }}
-   >
+ <Auth0Provider
+    domain="dev-3a3jih75x0t5i6tv.us.auth0.com"
+    clientId="Fox4p4S5LMtWOOChnfobgX0Cc0Dzvvmc"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
    <App />
    </Auth0Provider>
    </CartProvider>
